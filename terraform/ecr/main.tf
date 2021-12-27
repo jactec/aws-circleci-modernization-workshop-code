ba5eb663c10d8f1fe0c551e143ea0eecc1fe03e1
jac <jac@c9>
2021-12-27 01:50:03 +0000

    create docker

--- terraform/ecr/main.tf
+++ terraform/ecr/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    organization = "" # Enter the Terraform Cloud Organization here
+    organization = "jactec" # Enter the Terraform Cloud Organization here
 
     workspaces {
       name = "ecr-aws-circleci"
